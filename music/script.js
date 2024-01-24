3434eb9415798375bb68baf6e7b0e214428c661e
n4nm4r <144350280+n4nm4r@users.noreply.github.com>
2024-01-24 14:14:27 +0000

    song melody

--- music/script.js
+++ music/script.js
@@ -1,18 +1,40 @@
 function playNoteWithSleep(note,duration){
     const melodyOutput = document.getElementById("meloldyOutput");
     
-    melodyOutput.innerHTML += '🎵 Play ${note}';
+    melodyOutput.innerHTML += `🎵 Play ${note}`;
 
 
     setTimeout( () =>{
-        melodyOutput.innerHTML += 'Sleep ${duration}s'
-
-
+        melodyOutput.innerHTML += `Sleep ${duration}s`;
     },duration *1000); //end timeout
 
 
 }///close function
 
+//Mallory's Melody
+
+playNoteWithSleep(72,0.25);
+
+playNoteWithSleep(76,0.25);
+
+playNoteWithSleep(76,0.25);
+
+playNoteWithSleep(72,0.25);
+
+playNoteWithSleep(83,0.25);
+
+playNoteWithSleep(74,0.25);
+
+playNoteWithSleep(83,0.25);
+
+playNoteWithSleep(79,0.25);
+
+playNoteWithSleep(84,0);
 
 
-playNoteWithSleep(72,0.25)
+function attachMP3(){
+    const audioPlayer =document.querySelector("audio");
+    audioPlayer.src="https://sonic-pi.mehackit.org/assets/audio/c-major-melody.mp3";
+}
+
+attachMP3();
